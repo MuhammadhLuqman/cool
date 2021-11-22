65496f49e70ddb593aebfd098d68bacc1da9e791
MuhammadhLuqman <muhammadh.luqman@gmail.com>
2021-11-22 21:57:05 +0530

    red

---
+++ src/gold/Green.java
@@ -0,0 +1,10 @@
+package gold;
+
+public static void class Greens() {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module blue {
+	requires java.desktop;
+}
